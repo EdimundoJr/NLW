1e79b7503cda45b4332d2900de9b871c7a855ca4
Edimundo Junior <juniorconds@gmail.com>
2022-05-09 22:51:12 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 350) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 1400
+}).reveal(`#home,
+ 
+ #home .status,
+ #services,
+ #services header,
+ #services .inside-box
+ #about,
+ #about header,
+ #about .content
+ `)
